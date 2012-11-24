1ea7cafe86ddcc9f5c5c72c3be0e9fa7a163e993
Petter Strandmark <petter.strandmark@gmail.com>
2012-11-24 13:13:58 +0100

    Header wrapping OpenMP's wall time clock

---
+++ include/spii/spii.h
@@ -0,0 +1,19 @@
+#ifndef SPII_H
+#define SPII_H
+
+#ifdef USE_OPENMP
+	#include <omp.h>
+#endif
+
+namespace {
+double wall_time()
+{
+	#ifdef USE_OPENMP
+		return omp_get_wtime();
+	#else
+		return 0.0;
+	#endif
+}
+}
+
+#endif SPII_H
