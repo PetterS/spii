e5cdfa7056ec9e36a6cac93aa524131492748adc
Petter Strandmark <petter.strandmark@gmail.com>
2013-05-05 22:02:52 +0200

    New large test suite.

---
+++ tests/large_suite_andrei.h
@@ -0,0 +1,209 @@
+// Petter Strandmark 2013.
+//
+// Test functions from
+// Neculai Andrei, An Unconstrained Optimization Test Functions Collection,
+// Advanced Modeling and Optimization, Volume 10, Number 1, 2008.
+//
+
+#define BEGIN_MODEL1(Model) \
+	struct Model \
+	{ \
+		template<typename R>  \
+		R operator()(const R* const x) const \
+		{ \
+			R value = 0;
+
+#define BEGIN_MODEL2(Model) \
+	struct Model \
+	{ \
+		template<typename R>  \
+		R operator()(const R* const x, const R* const y) const \
+		{ \
+			R value = 0;
+
+#define END_MODEL \
+			return value; \
+		} \
+	}; 
+
+#define LARGE_SUITE_BEGIN(Model) \
+	TEST_CASE(#Model, "") \
+	{ \
+		auto create_function = [](std::vector<double>& start) -> Function \
+		{ \
+			int n = start.size(); \
+			Function f;
+#define LARGE_SUITE_MIDDLE \
+			return f; \
+		}; \
+		auto start_value = [](int n) -> std::vector<double> \
+		{ \
+			std::vector<double> value(n);
+#define LARGE_SUITE_END(test_newton) \
+			return value; \
+		}; \
+		run_test(create_function, start_value, test_newton); \
+	}
+
+const bool all_methods = true;
+const bool no_newton = false;
+
+BEGIN_MODEL1(FreudenSteinRoth)
+	R d1 = R(-13) + x[0] + ((R(5) - x[1])*x[1] - 2)*x[1];
+	R d2 = R(-29) + x[0] + ((x[1] + R(1))*x[1] - R(14))*x[1];
+	value += d1*d1 + d2*d2;
+END_MODEL
+
+LARGE_SUITE_BEGIN(FreudenSteinRoth)
+	for (int i = 0; i < n/2; ++i) {
+		f.add_variable(&start[2*i], 2);
+	}
+	for (int i = 0; i < n/2; ++i) {
+		f.add_term(new AutoDiffTerm<FreudenSteinRoth, 2>(
+			new FreudenSteinRoth), &start[2*i]);
+	}
+LARGE_SUITE_MIDDLE
+	for (int i = 0; i < n; ++i) {
+		if (i%2 == 0) {
+			value[i] = 0.5;
+		}
+		else {
+			value[i]  = -2.0;
+		}
+	}
+LARGE_SUITE_END(all_methods)
+
+// Not possible to define the Trigonometric function without a
+// custom Term.
+
+BEGIN_MODEL1(Rosenbrock)
+	R d1 = 10 * (x[1] - x[0]*x[0]);
+	R d2 = 1 - x[0];
+	value += d1*d1 + d2*d2;
+END_MODEL
+
+LARGE_SUITE_BEGIN(Rosenbrock)
+	for (int i = 0; i < n/2; ++i) {
+		f.add_variable(&start[2*i], 2);
+	}
+	for (int i = 0; i < n/2; ++i) {
+		f.add_term(new AutoDiffTerm<Rosenbrock, 2>(
+			new Rosenbrock), &start[2*i]);
+	}
+LARGE_SUITE_MIDDLE
+	for (int i = 0; i < n; ++i) {
+		if (i%2 == 0) {
+			value[i] = -1.2;
+		}
+		else {
+			value[i]  = 1.0;
+		}
+	}
+LARGE_SUITE_END(all_methods)
+
+BEGIN_MODEL1(WhiteHolst)
+	R d1 = 10 * (x[1] - x[0]*x[0]*x[0]);
+	R d2 = 1 - x[0];
+	value += d1*d1 + d2*d2;
+END_MODEL
+
+LARGE_SUITE_BEGIN(WhiteHolst)
+	for (int i = 0; i < n/2; ++i) {
+		f.add_variable(&start[2*i], 2);
+	}
+	for (int i = 0; i < n/2; ++i) {
+		f.add_term(new AutoDiffTerm<WhiteHolst, 2>(
+			new WhiteHolst), &start[2*i]);
+	}
+LARGE_SUITE_MIDDLE
+	for (int i = 0; i < n; ++i) {
+		if (i%2 == 0) {
+			value[i] = -1.2;
+		}
+		else {
+			value[i]  = 1.0;
+		}
+	}
+LARGE_SUITE_END(all_methods)
+
+// ...
+
+/*
+BEGIN_MODEL2(FLETCBV3a)
+	value = 0.5 * (x[0]*x[0] + y[0]*y[0]);
+END_MODEL
+
+BEGIN_MODEL2(FLETCBV3b)
+	R d = x[0] - y[0];
+	value = 0.5 * d*d;
+END_MODEL
+
+struct FLETCBV3c 
+{ 
+	double h;
+	FLETCBV3c(double _h) : h(_h) { }
+	template<typename R> 
+	R operator()(const R* const x) const
+	{
+		R value = 0;
+		value -= (h*h + 2.0) / (h*h) * x[0] + 1.0 / (h*h) * cos(x[0]);
+END_MODEL
+
+LARGE_SUITE_BEGIN(FLETCBV3)
+	for (int i = 0; i < n; ++i) {
+		f.add_variable(&start[i], 1);
+	}
+	f.add_term(new AutoDiffTerm<FLETCBV3a, 1, 1>(
+		new FLETCBV3a), &start[0], &start[n-1]);
+
+	for (int i = 0; i < n-1; ++i) {
+		f.add_term(new AutoDiffTerm<FLETCBV3b, 1, 1>(
+			new FLETCBV3b), &start[i], &start[i+1]);
+	}
+
+	for (int i = 0; i < n; ++i) {
+		f.add_term(new AutoDiffTerm<FLETCBV3c, 1>(
+			new FLETCBV3c(1.0)), &start[i]);
+	}
+LARGE_SUITE_MIDDLE
+	const double h = 1.0 / (n + 1.0);
+	for (int i = 0; i < n; ++i) {
+		value[i] = (i+1) * h;
+	}
+LARGE_SUITE_END(all_methods)
+*/
+
+BEGIN_MODEL1(TRIDIA1)
+	R d = x[0] - 1.0;
+	value = d*d;
+END_MODEL
+
+struct TRIDIA2 
+{ 
+	double i;
+	TRIDIA2(double _i) : i(_i) { }
+	template<typename R> 
+	R operator()(const R* const x, const R* const y) const
+	{
+		R value = 0;
+		R d = 2.0 * y[0] - x[0];
+		value = i * d*d;
+END_MODEL
+
+LARGE_SUITE_BEGIN(TRIDIA)
+	for (int i = 0; i < n; ++i) {
+		f.add_variable(&start[i], 1);
+	}
+	f.add_term(new AutoDiffTerm<TRIDIA1, 1>(
+		new TRIDIA1), &start[0]);
+
+	for (int i = 1; i < n; ++i) {
+		f.add_term(new AutoDiffTerm<TRIDIA2, 1, 1>(
+			new TRIDIA2(i + 1)), &start[i-1], &start[i]);
+	}
+
+LARGE_SUITE_MIDDLE
+	for (int i = 0; i < n; ++i) {
+		value[i] = 1.0;
+	}
+LARGE_SUITE_END(all_methods)
--- tests/test_suite_large.cpp
+++ tests/test_suite_large.cpp
@@ -19,344 +19,53 @@ void info_log_function(const std::string& str)
 	INFO(str);
 }
 
-// "An analysis of the behavior of a glass of genetic adaptive systems."
-// K.A. De Jong.  Ph.D. thesis, University of Michigan, 1975.
-struct GeneralizedRosenbrockTerm
+void run_test_main(const std::function<Function(std::vector<double>&)>& function,
+                   const std::function<std::vector<double>(int)>& start, 
+                   int n,
+                   Solver::Method method)
 {
-	template<typename R>
-	R operator()(const R* const x1, const R* const x2) const
-	{
-		R d0 =  (*x1) * (*x1) - (*x2);
-		R d1 =  1 - (*x1);
-		return 100 * d0*d0 + d1*d1;
-	}
-};
-
-// An easier variant.
-struct EasyRosenbrockTerm
-{
-	template<typename R>
-	R operator()(const R* const x1, const R* const x2) const
-	{
-		R d0 =  (*x1) * (*x1) - (*x2);
-		R d1 =  1 - (*x1);
-		return d0*d0 + d1*d1;
-	}
-};
-
-template<typename Functor, size_t n, bool lbfgs>
-void test_rosenbrock()
-{
-	Function f;
-	std::vector<double> x(n);
-	for (size_t i = 0; i < n; ++i) {
-		f.add_variable(&x[i], 1);
-	}
-
-	// Initial values.
-	for (size_t i = 0; i < n; ++i) {
-		if (i % 2 == 0) {
-			x[i] = -1.2;
-		}
-		else {
-			x[i] = 1.0;
-		}
-	}
-
-	// Add all diagonal terms.
-	for (size_t i = 0; i < n - 1; ++i) {
-		f.add_term(new AutoDiffTerm<Functor, 1, 1>
-		               (new Functor()), &x[i], &x[i+1]);
-	}
+	auto this_start = start(n);
+	auto f = function(this_start);
+	REQUIRE(f.get_number_of_scalars() == this_start.size());
 
 	Solver solver;
-	solver.maximum_iterations = 10000;
 	solver.log_function = info_log_function;
+	solver.function_improvement_tolerance = 0;
+	solver.argument_improvement_tolerance = 0;
+	solver.gradient_tolerance = 1e-7;
+	solver.maximum_iterations = 1000000;
+
 	SolverResults results;
-	if (lbfgs) {
-		solver.solve_lbfgs(f, &results);
-	}
-	else {
-		solver.solve_newton(f, &results);
-	}
-	INFO(results);
+	solver.solve(f, method, &results);
 
-	EXPECT_TRUE(results.exit_condition == SolverResults::ARGUMENT_TOLERANCE ||
-	            results.exit_condition == SolverResults::FUNCTION_TOLERANCE ||
-	            results.exit_condition == SolverResults::GRADIENT_TOLERANCE);
-
-	EXPECT_LT( std::fabs(f.evaluate()), 1e-9);
-
-	for (size_t i = 0; i < n - 1; ++i) {
-		ASSERT_LT( std::fabs(x[i] - 1.0), 1e-9);
-	}
+	CHECK(results.exit_success());
 }
 
-
-TEST(Newton, EasyRosenbrock1000)
+void run_test(const std::function<Function(std::vector<double>&)>& function,
+              const std::function<std::vector<double>(int)>& start,
+              bool test_newton = true)
 {
-	test_rosenbrock<EasyRosenbrockTerm, 1000, false>();
-}
-
-TEST(Newton, EasyRosenbrock10000)
-{
-	test_rosenbrock<EasyRosenbrockTerm, 10000, false>();
-}
-
-TEST(LBFGS, EasyRosenbrock1000)
-{
-	test_rosenbrock<EasyRosenbrockTerm, 1000, true>();
-}
-
-TEST(LBFGS, EasyRosenbrock10000)
-{
-	test_rosenbrock<EasyRosenbrockTerm, 10000, true>();
-}
-
-
-struct LennardJones
-{
-	template<typename R>
-	R operator()(const R* const p1, const R* const p2) const
-	{
-		R dx = p1[0] - p2[0];
-		R dy = p1[1] - p2[1];
-		R dz = p1[2] - p2[2];
-		R r2 = dx*dx + dy*dy + dz*dz;
-		R r6  = r2*r2*r2;
-		R r12 = r6*r6;
-		return 1.0 / r12 - 1.0 / r6;
-	}
-};
-
-template<bool lbfgs, int n>
-void test_Lennard_Jones()
-{
-	std::mt19937 prng(0);
-	std::normal_distribution<double> normal;
-	auto randn = std::bind(normal, prng);
-
-	Function potential;
-
-	std::vector<Eigen::Vector3d> points(n*n*n);
-	for (int x = 0; x < n; ++x) {
-		for (int y = 0; y < n; ++y) {
-			for (int z = 0; z < n; ++z) {
-				points[x + y*n + z*n*n][0] = x + 0.05 * randn();
-				points[x + y*n + z*n*n][1] = y + 0.05 * randn();
-				points[x + y*n + z*n*n][2] = z + 0.05 * randn();
-
-				potential.add_variable(&points[x + y*n + z*n*n][0], 3);
-			}
+	if (test_newton) {
+		SECTION("Newton-100", "") {
+			run_test_main(function, start, 100, Solver::NEWTON);
+		}
+		SECTION("Newton-1000", "") {
+			run_test_main(function, start, 1000, Solver::NEWTON);
+		}
+		SECTION("Newton-10000", "") {
+			run_test_main(function, start, 10000, Solver::NEWTON);
 		}
 	}
 
-	// Add all pairwise terms
-	for (int i = 0; i < n*n*n; ++i) {
-		for (int j = i + 1; j < n*n*n; ++j) {
-			potential.add_term(
-				new AutoDiffTerm<LennardJones, 3, 3>(
-					new LennardJones),
-					&points[i][0],
-					&points[j][0]);
-		}
+	SECTION("LBFGS-100", "") {
+		run_test_main(function, start, 100, Solver::LBFGS);
 	}
-
-	Solver solver;
-	solver.maximum_iterations = 1000;
-	solver.function_improvement_tolerance = 1e-6;
-	solver.log_function = info_log_function;
-
-	// All points interact with all points, so the Hessian
-	// will be dense.
-	solver.sparsity_mode = Solver::DENSE;
-	SolverResults results;
-	if (lbfgs) {
-		solver.solve_lbfgs(potential, &results);
+	SECTION("LBFGS-1000", "") {
+		run_test_main(function, start, 1000, Solver::LBFGS);
 	}
-	else {
-		solver.solve_newton(potential, &results);
-	}
-	INFO(results);
-	std::stringstream sout;
-	potential.print_timing_information(sout);
-	INFO(sout.str());
-
-	EXPECT_TRUE(results.exit_condition == SolverResults::ARGUMENT_TOLERANCE ||
-	            results.exit_condition == SolverResults::FUNCTION_TOLERANCE ||
-	            results.exit_condition == SolverResults::GRADIENT_TOLERANCE);
-}
-
-TEST(Newton, LennardJones)
-{
-	test_Lennard_Jones<false, 5>();
-}
-
-TEST(LBFGS, LennardJones)
-{
-	test_Lennard_Jones<true, 5>();
-}
-
-struct Trid1
-{
-	template<typename R>
-	R operator()(const R* const x) const
-	{
-		R d = *x - 1.0;
-		return d*d;
-	}
-};
-
-struct Trid2
-{
-	template<typename R>
-	R operator()(const R* const x1, const R* const x2) const
-	{
-		return - (*x1) * (*x2);
-	}
-};
-
-template<size_t n, bool lbfgs>
-void test_trid()
-{
-	Function f;
-
-	std::vector<double> x(n, 1.0);
-	for (size_t i = 0; i < n; ++i) {
-		f.add_variable(&x[i], 1);
-	}
-
-	for (size_t i = 0; i < n; ++i) {
-		f.add_term( new AutoDiffTerm<Trid1, 1>(
-			new Trid1),
-			&x[i]);
-	}
-
-	for (size_t i = 1; i < n; ++i) {
-		f.add_term( new AutoDiffTerm<Trid2, 1, 1>(
-			new Trid2),
-			&x[i],
-			&x[i-1]);
-	}
-
-	Solver solver;
-	solver.maximum_iterations = 100;
-	//solver.argument_improvement_tolerance = 1e-16;
-	//solver.gradient_tolerance = 1e-16;
-	solver.log_function = info_log_function;
-
-	SolverResults results;
-	if (lbfgs) {
-		solver.maximum_iterations = 1000;
-		solver.solve_lbfgs(f, &results);
-	}
-	else {
-		solver.solve_newton(f, &results);
-	}
-	INFO(results);
-
-	double fval = f.evaluate();
-	// Global optimum is
-	//
-	//   x[i] = (i + 1) * (n - i)
-	//
-	// Therefore, it is hard to calulate the optimal function
-	// value for large n.
-	//
-	double tol  = 1e-8;
-	if (n >= 10000) {
-		tol = 1e-6;
-	}
-	if (n >= 100000) {
-		tol = 1e-4;
-	}
-	double nn = n;
-	EXPECT_LT(std::fabs(fval + (nn * (nn+4) * (nn-1)) / 6.0) / std::abs(fval), tol);
-	EXPECT_TRUE(results.exit_condition == SolverResults::ARGUMENT_TOLERANCE ||
-	            results.exit_condition == SolverResults::FUNCTION_TOLERANCE ||
-	            results.exit_condition == SolverResults::GRADIENT_TOLERANCE);
-
-	if (n <= 10) {
-		for (size_t i = 0; i < n; ++i) {
-			INFO("x[" << i << "] = " << x[i]);
-		}
+	SECTION("LBFGS-10000", "") {
+		run_test_main(function, start, 10000, Solver::LBFGS);
 	}
 }
 
-TEST(Newton, Trid10)
-{
-	test_trid<10, false>();
-}
-
-TEST(Newton, Trid1000)
-{
-	test_trid<1000, false>();
-}
-
-TEST(Newton, Trid10000)
-{
-	test_trid<10000, false>();
-}
-
-struct LogBarrier01
-{
-	template<typename R>
-	R operator()(const R* const x) const
-	{
-		return -log(x[0]) - log(1.0 - x[0]);
-	}
-};
-
-struct QuadraticFunction1
-{
-	QuadraticFunction1(double b)
-	{
-		this->b = b;
-	}
-
-	template<typename R>
-	R operator()(const R* const x) const
-	{
-		R d = x[0] - b;
-		return d * d;
-	}
-
-	double b;
-};
-
-TEST(Newton, Barrier)
-{
-	std::mt19937 prng(0);
-	std::normal_distribution<double> normal;
-	auto randn = std::bind(normal, prng);
-
-	const int n = 10000;
-	Function f;
-
-	std::vector<double> x(n, 0.5);
-	for (size_t i = 0; i < n; ++i) {
-		f.add_variable(&x[i], 1);
-		f.add_term( new AutoDiffTerm<QuadraticFunction1, 1>(
-			new QuadraticFunction1(0.5 + randn())),
-			&x[i]);
-		f.add_term( new AutoDiffTerm<LogBarrier01, 1>(
-			new LogBarrier01),
-			&x[i]);
-	}
-
-	Solver solver;
-	solver.maximum_iterations = 100;
-	solver.log_function = info_log_function;
-
-	SolverResults results;
-	solver.solve_newton(f, &results);
-
-	INFO(results);
-	std::stringstream sout;
-	f.print_timing_information(sout);
-	INFO(sout.str());
-
-	EXPECT_TRUE(results.exit_condition == SolverResults::ARGUMENT_TOLERANCE ||
-	            results.exit_condition == SolverResults::FUNCTION_TOLERANCE ||
-	            results.exit_condition == SolverResults::GRADIENT_TOLERANCE);
-}
+#include "large_suite_andrei.h"
