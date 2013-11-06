d859107b1194e75d9ce409d562e16c7787bea410
Petter Strandmark <petter.strandmark@gmail.com>
2013-11-06 20:56:22 +0100

    LBFGS: Do not check exit condition the first iteration.

--- source/solver_lbfgs.cpp
+++ source/solver_lbfgs.cpp
@@ -122,9 +122,9 @@ void LBFGSSolver::solve(const Function& function,
 		// Test stopping criteriea
 		//
 		start_time = wall_time();
-		if (this->check_exit_conditions(fval, fprev, normg,
-		                                normg0, x.norm(), normdx,
-		                                last_iteration_successful, results)) {
+		if (iter > 1 && this->check_exit_conditions(fval, fprev, normg,
+		                                            normg0, x.norm(), normdx,
+		                                            last_iteration_successful, results)) {
 			break;
 		}
 		if (iter >= this->maximum_iterations) {
