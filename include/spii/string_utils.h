768e34aec243f35e5560b1d62caa13075ebf050b
Petter Strandmark <petter.strandmark@gmail.com>
2013-12-26 23:36:49 +0100

    Fix string_utils.h and add basic format_string
    The order of the functions is important to gcc.

--- include/spii/string_utils.h
+++ include/spii/string_utils.h
@@ -2,6 +2,7 @@
 #ifndef SPII_STRING_UTILS_H
 #define SPII_STRING_UTILS_H
 
+#include <ostream>
 #include <sstream>
 #include <stdexcept>
 #include <string>
@@ -17,6 +18,9 @@ namespace spii
 // template recursion is a normal function.
 namespace {
 
+	void add_to_stream(std::ostream*)
+	{  }
+
 	template<typename T, typename... Args>
 	void add_to_stream(std::ostream* stream, T&& t, Args&&... args)
 	{
@@ -24,8 +28,17 @@ namespace {
 		add_to_stream(stream, std::forward<Args>(args)...);
 	}
 
-	void add_to_stream(std::ostream*)
-	{  }
+	std::string to_string()
+	{
+		return{};
+	}
+
+	// Overload for string literals.
+	template<size_t n>
+	std::string to_string(const char(&c_str)[n])
+	{
+		return{c_str};
+	}
 
 	template<typename... Args>
 	std::string to_string(Args&&... args)
@@ -34,20 +47,77 @@ namespace {
 		add_to_stream(&stream, std::forward<Args>(args)...);
 		return stream.str();
 	}
+}
 
-	std::string to_string()
-	{ 
-		return {};
-	}
 
-	// Overload for string literals.
-	template<size_t n>
-	std::string to_string(const char(&c_str)[n])
+namespace
+{
+	std::ostream& format_string_internal(std::ostream& stream,
+	                                     const char* str,
+	                                     const std::vector<std::string>& arguments)
 	{
-		return{ c_str };
+		while (*str) {
+			if (*str == '%') {
+				++str;
+				if (*(str) == '%') {
+					// OK. This will result in "%".
+					stream << '%';
+					++str;
+				}
+				else {
+					int digit = *str - '0';
+					++str;
+					if (digit < 0 || digit > 9) {
+						throw std::invalid_argument("Format specifier must be in {0, ..., 9}.");
+					}
+					if (digit >= arguments.size()) {
+						throw std::invalid_argument("Too few arguments to format_string.");
+					}
+
+					stream << arguments.at(digit);
+
+					// To allow format specifiers of the type "%0%".
+					//if (*str == '%') {
+					//	++str;
+					//}
+				}
+			}
+			else {
+				stream << *str;
+				++str;
+			}
+		}
+		return stream;
 	}
 }
 
+
+template<typename... Args>
+std::ostream& format_string(std::ostream& stream, const char* str, Args&&... args)
+{
+	std::vector<std::string> arguments = {to_string(std::forward<Args>(args))...};
+	return format_string_internal(stream, str, arguments);
+}
+
+template<typename... Args>
+std::ostream& format_string(std::ostream& stream, const std::string& str, Args&&... args)
+{
+	return format_string(stream, str.c_str(), std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+std::string format_string(const char* str, Args&&... args)
+{
+	std::ostringstream stream;
+	format_string(stream, str, std::forward<Args>(args)...);
+	return stream.str();
+}
+
+template<typename... Args>
+std::string format_string(const std::string& str, Args&&... args)
+{
+	return format_string(str.c_str(), std::forward<Args>(args)...);
+}
 
 }
 
--- tests/test_string.cpp
+++ tests/test_string.cpp
@@ -12,3 +12,23 @@ TEST_CASE("to_string")
 {
 	CHECK(to_string("Test",12,"test") == "Test12test");
 }
+
+TEST_CASE("format_string_1")
+{
+	CHECK(format_string("Test %0!", 12) == "Test 12!");
+	CHECK(format_string("Test %0", 12) == "Test 12");
+}
+
+TEST_CASE("format_string_%")
+{
+	CHECK(format_string("Test %0%%", 12) == "Test 12%");
+	CHECK(format_string("Test %0%%!", 12) == "Test 12%!");
+	CHECK(format_string("Test %0 %%", 12) == "Test 12 %");
+	CHECK(format_string("Test %0 %%!", 12) == "Test 12 %!");
+}
+
+TEST_CASE("format_string_2")
+{
+	CHECK(format_string("Test %0 and %1!", 'A', 'O') == "Test A and O!");
+	CHECK(format_string("Test %1 and %0!", 'A', 'O') == "Test O and A!");
+}
