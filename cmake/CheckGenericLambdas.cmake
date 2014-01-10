50e7f62b34f1a18dc9e15dab351edb12ce76b8f7
Petter Strandmark <petter.strandmark@gmail.com>
2014-01-10 18:27:43 +0100

    Add generic lambda preprocessor definition.

--- cmake/CheckGenericLambdas.cmake
+++ cmake/CheckGenericLambdas.cmake
@@ -4,6 +4,7 @@ CHECK_CXX_SOURCE_COMPILES("int main() { auto lambda = [](auto x, auto y) { retur
 
 IF (USE_GENERIC_LAMBDAS)
 	MESSAGE("-- Generic lambdas (C++14) are supported by the compiler.")
+	ADD_DEFINITIONS(-DUSE_GENERIC_LAMBDAS)
 ELSE ()
 	MESSAGE("-- Generic lambdas (C++14) are not supported by the compiler.")
 ENDIF()
