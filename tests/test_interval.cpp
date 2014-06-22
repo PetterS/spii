485e2eb2553e474679112be0cd5babc3a0aef194
Petter Strandmark <petter.strandmark@gmail.com>
2014-06-22 20:46:51 +0200

    Add interval tests.

--- tests/test_interval.cpp
+++ tests/test_interval.cpp
@@ -1,4 +1,4 @@
-// Petter Strandmark 2013.
+// Petter Strandmark 2013–2014.
 
 #include <iostream>
 
@@ -8,7 +8,7 @@
 #include <spii/interval.h>
 using namespace spii;
 
-TEST_CASE("Interval/get_set", "Tests get and set functions")
+TEST_CASE("get")
 {
 	Interval<double> i1(123, 456);
 
@@ -16,7 +16,20 @@ TEST_CASE("Interval/get_set", "Tests get and set functions")
 	CHECK(i1.get_upper() == 456);
 }
 
-TEST_CASE("Interval/plus", "")
+TEST_CASE("length")
+{
+	Interval<int> i1(123, 456);
+
+	CHECK(i1.length() == 456 - 123);
+}
+
+TEST_CASE("invalid_construction")
+{
+	CHECK_THROWS(Interval<double>(2, 1));
+	CHECK_NOTHROW(Interval<int>(1, 1));
+}
+
+TEST_CASE("plus")
 {
 	Interval<double> i1(1, 2);
 	Interval<double> i2(3, 4);
@@ -27,7 +40,7 @@ TEST_CASE("Interval/plus", "")
 	CHECK((i1 + i2) == Interval<double>(4, 6));
 }
 
-TEST_CASE("Interval/minus", "")
+TEST_CASE("minus")
 {
 	Interval<double> i1(1, 2);
 	Interval<double> i2(3, 4);
@@ -38,7 +51,7 @@ TEST_CASE("Interval/minus", "")
 	CHECK((i1 - i2) == -(i2 - i1));
 }
 
-TEST_CASE("Interval/multiplication", "")
+TEST_CASE("multiplication")
 {
 	Interval<double> i1(1, 2);
 
@@ -71,7 +84,7 @@ TEST_CASE("multiplication_all_sign_combinations")
 	}}
 }
 
-TEST_CASE("Interval/division", "")
+TEST_CASE("division")
 {
 	Interval<double> result;
 
@@ -138,7 +151,7 @@ TEST_CASE("division_as_multiplication")
 	}}
 }
 
-TEST_CASE("Interval/cos", "")
+TEST_CASE("cos")
 {
 	Interval<double> result;
 
@@ -168,6 +181,23 @@ TEST_CASE("Interval/cos", "")
 	CHECK(Approx(result.get_upper()) == cos(2.0));
 }
 
+TEST_CASE("sin")
+{
+	using namespace std;
+
+	Interval<double> i1(-10.0, 10.0);
+	CHECK(sin(i1) == Interval<double>(-1.0, 1.0));
+	Interval<double> i2(3.0, 3.0);
+	CHECK(Approx(sin(i2).get_lower()) == sin(3.0));
+	CHECK(Approx(sin(i2).get_upper()) == sin(3.0));
+}
+
+TEST_CASE("abs")
+{
+	Interval<double> i1(-10.0, 10.0);
+	CHECK(abs(sin(i1)) == Interval<double>(0.0, 1.0));
+}
+
 TEST_CASE("odd_powers")
 {
 	using std::pow;
@@ -186,4 +216,32 @@ TEST_CASE("even_powers")
 
 	Interval<double> i3(-2.0, 3.0);
 	CHECK(pow(i3, 2) == Interval<double>(0, 9));
+}
+
+// Rump’s example.
+// See section 1.4.1 in “Global Optimization Using Interval Analysis” by Eldon Hansen.
+template<typename R>
+R rump(R x, R y)
+{
+	using std::pow;
+	return (333.75 - pow(x, 2)) * pow(y, 6)
+		+ pow(x, 2) * (11.0 * pow(x, 2) * pow(y, 2) - 121.0 * pow(y, 4) - 2.0)
+		+ 5.5 * pow(y, 8) + x / (2.0 * y);
+}
+
+TEST_CASE("Rump")
+{
+	double x0 = 77617;
+	double y0 = 33096;
+	double gt = -0.82739605994682136814116509;
+	
+	// Fails due to the difficulty of Rump’s function.
+	//CHECK(Approx(rump(x0, y0)) == gt);
+
+	double eps = 1e-14;
+	Interval<double> ix(x0 - x0*eps, x0 + x0*eps);
+	Interval<double> iy(y0 - y0*eps, y0 + y0*eps);
+	CAPTURE(rump(ix, iy));  // Really wide!
+	CHECK(rump(ix, iy).get_lower() <= gt);
+	CHECK(rump(ix, iy).get_upper() >= gt);
 }
